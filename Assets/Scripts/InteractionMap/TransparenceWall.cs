923a92ba9f9468e714a4a5daf2480ea144bc930e
juliaazeved0 <juliacarolineazevedo@gmail.com>
2026-01-15 12:06:28 -0300

    feat: add TransparenceWall script

---
+++ Assets/Scripts/InteractionMap/TransparenceWall.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TransparenceWall : MonoBehaviour
+{
+    private TilemapRenderer tilemapRenderer;
+    private Tilemap tilemap;
+
+    void Start()
+    {
+        
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            Color c = tilemap.color;
+            c.a = 0.5f;
+            tilemap.color = c;
+
+            
+            tilemapRenderer.sortingOrder = 10; 
+        }
+    }
+
+    //private void OnTriggerStay2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //    
+    //        Color c = tilemap.color;
+    //        if(c.a != 0.5f) 
+    //        {
+    //            c.a = 0.5f;
+    //            tilemap.color = c;
+    //            tilemapRenderer.sortingOrder = 10;
+    //        }
+    //    }
+//}
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            Color c = tilemap.color;
+            c.a = 1f;
+           tilemap.color = c;
+    
+            
+            tilemapRenderer.sortingOrder = 0; 
+        }
+    }
+}
+
