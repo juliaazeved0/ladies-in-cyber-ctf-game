f6c6d8d111c843322ba48aaae2b486dc87fdfe4a
Polyana Neuland <neulandpoly@gmail.com>
2026-02-09 10:24:23 -0300

    add: Felipe's computer password and implemented script

--- Assets/Scripts/ValidatePassword.cs
+++ Assets/Scripts/ValidatePassword.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro; //Importa as ferramentas do TextMeshPro
+using TMPro; //Importa as bibliotecas para usar o TextMeshPro
 
 public class ValidatePassword : MonoBehaviour
 {
     public TMP_InputField passwordField; //Campo onde o jogador digita a senha do computador
     public GameObject errorMessage; //Variável para arrastar o objeto de texto da "senha inválida"
-    public string correctPassword = "1234"; //A senha correta
+    public string correctPassword = "MUDAR123"; //A string que define a senha correta
+
+    void Start()
+    {
+        errorMessage.SetActive(false); //Garante que o pop-up de erro comece escondido
+        passwordField.ActivateInputField(); //Faz com que o cursos já apareça piscando dentro do Input Field, sem o jogador precisar clicar
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) //Verifica se a tecla Enter principal ou do numérico foi apertada
+        {
+            CheckPassword(); //Se apertou no Enter, chama a função que valida a senha
+        }
+    }
 
     public void CheckPassword()
     {
-        if (passwordField.text == correctPassword) //Se o texto digitado for exatamente igual  senha
+        if (passwordField.text == correctPassword) //Verifica se o texto escrito no campo é igual ao da variável
         {
-            Debug.Log("Senha Certa!"); //Teste para mostrar a senha correta
-            errorMessage.SetActive(false); //Esconde a mensagem de erro
+            errorMessage.SetActive(false); //Se for igual, esconde a mensagem de erro
+            // Lógica de sucesso aqui (ex: abrir porta)
         }
-        else //Se a senha estiver errada
+        else
         {
-            errorMessage.SetActive(true); //Ativa a mensagem de mensagem
+            errorMessage.SetActive(true); //Mostra a mensagem de erro
+            passwordField.text = ""; //Apaga o que o jogador digitou para ele tentar novamente
+            passwordField.ActivateInputField(); //Devolve o foco do teclado para o campo automaticamente
         }
     }
 }
