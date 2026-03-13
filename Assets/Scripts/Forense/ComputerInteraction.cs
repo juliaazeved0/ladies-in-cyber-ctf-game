6430f610a7561a15babda98892b69fb0f107169e
Polyana Neuland <neulandpoly@gmail.com>
2026-03-13 14:17:17 -0300

    add and feat: added images, script comments, and the rest of the black box challenge implemented

--- Assets/Scripts/Forense/ComputerInteraction.cs
+++ Assets/Scripts/Forense/ComputerInteraction.cs
@@ -13,36 +13,36 @@ public class ComputerInteraction : MonoBehaviour
     [Header("Sistema do Desafio")]
     public GameObject initialBackground; //Painel do início do desafio
 
-    private bool playerIsNear = false; //Controla se a jogadora está na área
+    private bool playerIsNear = false; //Controla se a jogadora está perto do computador
 
      void Update()
     {
-        if(playerIsNear && Input.GetKeyDown(KeyCode.E))
+        if(playerIsNear && Input.GetKeyDown(KeyCode.E)) //Verifica se a jogadora está perto do computador e se pressionou a tecla E
         {
-            if(initialBackground != null)
+            if(initialBackground != null) //Faz o painel aparecer na tela
             {
                 initialBackground.SetActive(true);
 
-                if(interactionNotice != null) interactionNotice.SetActive(false);
+                if(interactionNotice != null) interactionNotice.SetActive(false); //Esconde o aviso de interação quando o painel do PC abre
             }
         } 
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) //Função chamada automaticamente quando a jogadora entra no collider marcado
     {
         if (collision.CompareTag("Player")) //Verifica se o personagem entrou na área de colisão
         {
-            playerIsNear = true;
+            playerIsNear = true; //Marca que a jogadora entrou na área, então, ela pode interagir
 
             if (scriptPulse != null) scriptPulse.StartPulsing(); //Liga o brilho ao redor do computador assim que a jogadora se aproxima
             if(interactionNotice != null) interactionNotice.SetActive(true); //Mostra o aviso de interação
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collision) //Função para quando a jogadora sai da área de interação
     {
-        if (collision.CompareTag("Player")) //Verifica se a perosnagem saiu da área de colisão
+        if (collision.CompareTag("Player")) //Verifica se a personagem saiu da área de colisão
         {
-            playerIsNear = false;
+            playerIsNear = false; //A jogadora não pode mais interagir com o computador
 
             if (scriptPulse != null) scriptPulse.StopPulsing(); //Desativa o brilho do computador assim que a jogadora se afasta
             if(interactionNotice != null) interactionNotice.SetActive(false); //Esconde o aviso de interação
--- Assets/Scripts/Forense/ServerController.cs
+++ Assets/Scripts/Forense/ServerController.cs
@@ -2,49 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ServerController : LockObjectInteraction
+public class ServerController : LockObjectInteraction //Já possui todas as funções de interação do LockObjectInteraction
 {
-    [Header("Objetos dos Cadeados")] //Título no Inspector para organizar e permite que arraste objetos
+    [Header("Objetos dos Cadeados")] //Título visual no Inspector e permite que arraste objetos
     public SpriteRenderer lockClosed; //Imagem do cadeado fechado
     public SpriteRenderer lockOpened; //Imagem do cadeado aberto
 
+    [Header("Estado Final")]
+    public bool finalizado = false; //Variável para indicar se o desafio do servidor foi concluído
+    public GameObject panelConnect; //Painel que aparece quando os cabos precisam ser conectados
+
     void Start()
     {
-        isUnlocked = false;
+        isUnlocked = false; //O servidor começa bloqueado
+        finalizado = false; //Garante que o desafio do servidor também começa não finalizado
 
+        //Verifica se o objeto foi arrastado no Inspector. Se foi, ativa o cadeado fechado. Ou seja, o servidor começa visualmente bloqueado
         if (lockClosed != null) lockClosed.gameObject.SetActive(true);
+
+        //Desativa o cadeado aberto. Assim, só aparece o cadeado fechado no início
         if(lockOpened != null) lockOpened.gameObject.SetActive(false);
 
+        //Painel que o desafio aparece quando o servidor é desbloqueado, sendo desativado no início
         if(challengePanel != null) challengePanel.gameObject.SetActive(false);
+
+        //Garante que o painel de conexão de cabos comece escondido
+        if (panelConnect != null) panelConnect.SetActive(false);
     }
 
     protected override void Update()
     {
-        base.Update();
+        base.Update(); //Detecta a tecla E, aparição do modal e a lógica de proximidade
     }
 
     public void UnlockByHacking() //Função chamada pelo ManagerPanels
     {
-        isUnlocked = true;
+        isUnlocked = true; //Marca o servidor como desbloqueado depois de ser acessado no PC
 
-        //Troca os cadeados visualmente
-        if (lockClosed != null) lockClosed.gameObject.SetActive(false);
-        if (lockOpened != null) lockOpened.gameObject.SetActive(true);
+        if (lockClosed != null) lockClosed.gameObject.SetActive(false); //Esconde o cadeado fechado
+
+        if (lockOpened != null) lockOpened.gameObject.SetActive(true); //Mostrado o cadeado aberto
     }
 
-    protected override void Interact()
+    protected override void Interact() //Função chamada quando a jogadora pressiona a tecla E perto do objeto
     {
-        // Se a jogadora interagir E o hack do PC já estiver liberado
+        if (finalizado) //A partir disso, quando a jogadora interagir novamente, mostra apenas o painel de conexão com os cabos
+        {
+            if(panelConnect != null)
+            {
+                panelConnect.SetActive(true); //Mostra o painel de conexão dos cabos
+
+                if (interactionNotice != null) interactionNotice.SetActive(false); //Esconde o modal
+            }
+            return; //Interrompe a função, nada mais abaixo é executado
+        }
+        
+        //Só permite a interação se o servidor for hackeado pelo PC
         if (isUnlocked)
         {
             if (challengePanel != null)
             {
-                challengePanel.SetActive(true);
+                challengePanel.SetActive(true); //Abre o painel do desafio do servidor
 
-                // Desativa o aviso visual "Aperte E" enquanto ela faz o desafio
+                //Desativa o aviso visual "Aperte E" enquanto ela faz o desafio
                 if (interactionNotice != null) interactionNotice.SetActive(false);
             }
 
         }
+    }
+
+    public void ConcluirServidor()
+    {
+        finalizado = true; //Marca o servidor como completado
     }
 }
