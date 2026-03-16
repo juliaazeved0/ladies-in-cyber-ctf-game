51f43f2ac4ac1f8731d6f8a8e2ead267b1a867eb
juliaazeved0 <juliacarolineazevedo@gmail.com>
2026-03-16 12:07:11 -0300

    added bag and playbook sprites

--- Assets/Scripts/UI/InventoryManager.cs
+++ Assets/Scripts/UI/InventoryManager.cs
@@ -4,28 +4,31 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-[Header("Configurações")]
-    public float velocidade = 10f; // Quão rápido a borda desliza
+[Header("Settings")]
+    public float velocity = 10f; 
 
-    // O objeto que a borda está perseguindo no momento
-    private RectTransform alvoAtual; 
+    private RectTransform currentButton; 
 
     void Update()
     {
-        // Se temos um alvo, a borda desliza suavemente até ele a cada frame
-        if (alvoAtual != null)
+        if (currentButton != null)
         {
-            transform.position = Vector3.Lerp(transform.position, alvoAtual.position, Time.deltaTime * velocidade);
+            transform.position = Vector3.Lerp(transform.position, currentButton.position, Time.deltaTime * velocity);
         }
     }
 
-    // Função que os botões vão chamar quando forem clicados
-    public void MoverParaBotao(RectTransform novoAlvo)
+    public void OnClickBag(){
+
+    }
+
+    public void OnClickPlayBook(){
+        
+    }
+
+    public void MoveToButton(RectTransform newButton)
     {
-        // Ativa a borda (caso ela comece invisível)
         gameObject.SetActive(true); 
         
-        // Define o novo alvo para a borda perseguir
-        alvoAtual = novoAlvo;
+        currentButton = newButton;
     }
 }
