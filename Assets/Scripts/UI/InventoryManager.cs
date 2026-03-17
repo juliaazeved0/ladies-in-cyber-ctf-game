15a02ffe87d6e7e29ba98735778cd22ba6addb0a
juliaazeved0 <juliacarolineazevedo@gmail.com>
2026-03-17 12:19:59 -0300

    feat: update InventoryManager scripts and UI inventory

--- Assets/Scripts/UI/InventoryManager.cs
+++ Assets/Scripts/UI/InventoryManager.cs
@@ -4,31 +4,61 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-[Header("Settings")]
+    [Header("Settings - Panels")]
+    public GameObject panelBag;
+    public GameObject panelPlayBook;
+    public GameObject panelBackgroundInventory;
+
+    [Header("Settings - Buttons & Border")]
+    public RectTransform buttonBag;
+    public RectTransform buttonPlayBook;
+    public RectTransform borderImage; 
+
     public float velocity = 10f; 
 
     private RectTransform currentButton; 
 
+    void Start()
+    {
+        panelBackgroundInventory.SetActive(false);
+    }
+
     void Update()
     {
-        if (currentButton != null)
+        if (currentButton != null && borderImage != null)
         {
-            transform.position = Vector3.Lerp(transform.position, currentButton.position, Time.deltaTime * velocity);
+            borderImage.position = Vector3.Lerp(borderImage.position, currentButton.position, Time.deltaTime * velocity);
         }
     }
 
-    public void OnClickBag(){
-
+    public void OnClickBag()
+    {
+        panelBackgroundInventory.SetActive(true);
+        //CanvasManager.Instance.OpenPanel(panelBackgroundInventory.name);
+        CanvasManager.Instance.OpenPanel(panelBag.name);
+        MoveToButton(buttonBag);
     }
 
-    public void OnClickPlayBook(){
-        
+    public void OnClickPlayBook()
+    {
+        panelBackgroundInventory.SetActive(true);
+        //CanvasManager.Instance.OpenPanel(panelBackgroundInventory.name);
+        CanvasManager.Instance.OpenPanel(panelPlayBook.name);
+        MoveToButton(buttonPlayBook);
+    }
+    
+
+    public void ExitInventory()
+    {
+        panelBackgroundInventory.SetActive(false);
+        CanvasManager.Instance.ClosedPanel(gameObject.name);
+        borderImage.gameObject.SetActive(false);
+        //CanvasManager.Instance.ClosedPanel(panelBackgroundInventory.name);
     }
 
     public void MoveToButton(RectTransform newButton)
     {
-        gameObject.SetActive(true); 
-        
+        borderImage.gameObject.SetActive(true); 
         currentButton = newButton;
     }
 }
